a3810f7336a50f190baca2b9db72da24b5336160
SabreT00th84 <91007752+SabreT00th84@users.noreply.github.com>
2026-04-30 16:26:06 +0100

    chore: Remove redundant RaceConfig record

--- src/main/java/Part2/Models/RaceConfig.java
+++
@@ -1,9 +0,0 @@
-package Part2.Models;
-
-public record RaceConfig(
-        String passage,
-        int numOfTypists,
-        boolean autocorrect,
-        boolean caffeine,
-        boolean night
-){}
